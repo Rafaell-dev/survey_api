071a59674637537ee7bb184c45a3a373fd526fdd
rafaelldev <rafaelaraujo.contact@gmail.com>
2026-07-02 23:35:09 -0300

    feat: implement get responses analytics endpoint and fix timezone bug

--- src/modules/surveys/controllers/analytics.controller.ts
+++ src/modules/surveys/controllers/analytics.controller.ts
@@ -4,6 +4,8 @@ import { GetOverviewService } from '../services/analytics/get-overview.service';
 import { GetQuestionsAnalyticsService } from '../services/analytics/get-questions-analytics.service';
 import { GetNavigationAnalyticsService } from '../services/analytics/get-navigation-analytics.service';
 import { GetMediaAnalyticsService } from '../services/analytics/get-media-analytics.service';
+import { GetResponsesAnalyticsService } from '../services/analytics/get-responses.service';
+import { ExportRepository } from '../repositories/export.repository';
 
 export class AnalyticsController {
   private repository = new AnalyticsRepository();
@@ -46,6 +48,22 @@ export class AnalyticsController {
       const researcherId = (request.user as any).sub;
       const service = new GetMediaAnalyticsService(this.repository);
       const result = await service.execute(request.params.surveyId, researcherId);
+      return reply.status(200).send(result);
+    } catch (err: any) {
+      return reply.status(err.status || 500).send({ message: err.message });
+    }
+  }
+
+  async getResponses(request: FastifyRequest<{ Params: { surveyId: string }, Querystring: any }>, reply: FastifyReply) {
+    try {
+      const researcherId = (request.user as any).sub;
+      const filters = typeof request.query.filters === 'string' 
+        ? JSON.parse(request.query.filters) 
+        : request.query;
+
+      const exportRepo = new ExportRepository();
+      const service = new GetResponsesAnalyticsService(exportRepo);
+      const result = await service.execute(request.params.surveyId, researcherId, filters);
       return reply.status(200).send(result);
     } catch (err: any) {
       return reply.status(err.status || 500).send({ message: err.message });
